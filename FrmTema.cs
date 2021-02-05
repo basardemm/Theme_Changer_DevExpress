96ec8e4c43ad9437f354e8efa6e86f868c66f6be
M. Başar Demirci <62293688+basardemm@users.noreply.github.com>
2021-02-05 14:49:47 +0300

    Add files via upload

---
+++ FrmTema.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.IO;
+using DevExpress.LookAndFeel;
+using DevExpress.Skins;
+
+namespace TeknikServis
+{
+    public partial class FrmTema : Kalitim
+    {
+        public FrmTema()
+        {
+            InitializeComponent();
+        }
+
+        #region Tema Seçici
+        private void FrmTema_Load(object sender, EventArgs e)
+        {
+            cmbTema.SelectedIndexChanged += CmbTema_SelectedIndexChanged;
+            foreach (SkinContainer item in SkinManager.Default.Skins)
+            {
+                cmbTema.Properties.Items.Add(item.SkinName);
+            }
+        }
+
+        private void CmbTema_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBoxEdit comboBox = sender as ComboBoxEdit;
+            string skinAdi = comboBox.Text;
+            UserLookAndFeel.Default.SkinName = skinAdi;
+        }
+        #endregion
+
+
+        #region Uygula Butonu
+        private void btnUygula_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("kartel_themes.ltd"))
+            {
+                sw.WriteLine(cmbTema.Text);
+                MessageBoxes.Information("Tema Kaydedildi");
+                this.Close();
+            }
+        } 
+        #endregion
+
+    }
+}
